0fa570f8fecec2b070c5fadce9c9f2497b21eecd
Chirag Prajapati <chiragprajapati6100@gmail.com>
2026-06-25 15:30:41 +0530

    added stats for requests

--- controllers/stats.js
+++ controllers/stats.js
@@ -3,17 +3,19 @@ const statsRouter = require('express').Router()
 const Donor = require('../models/donors')
 const BloodBank = require('../models/bloodbanks')
 const Hospital = require('../models/hospitals')
+const Request = require('../models/requests')
 
 statsRouter.get('/', async (req, res) => {
     try{
         let totalDonors = await Donor.countDocuments()
         let totalHospitals = await Hospital.countDocuments()
         let totalBloodBank = await BloodBank.countDocuments()
-
+        let totalRequests = await Request.countDocuments({status: "completed"})
         res.json({
             totalDonors,
             totalHospitals,
-            totalBloodBank
+            totalBloodBank,
+            totalRequests 
         })
     } catch (error) {
     res.status(500).json({
